423343d98a1fc5e334ee4aae335ebe18ea486c6c
Tess Barnes <tess.barnes@justice.gov.uk>
2025-02-24 10:10:43 +0000

    Add basic authentication to deter public use of the prototype

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,35 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import httpAuth from "vite-plugin-http-basic-auth"
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return ({
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === 'development' &&
+      componentTagger(),
+      httpAuth([{
+        username: env.VITE_AUTH_USERNAME_1,
+        password: env.VITE_AUTH_PASSWORD_1
+      }], {
+          realm: env.VITE_AUTH_REALM,
+          useInServer: true,   // True by default
+          useInPreview: true,  // True by default
+      })
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  })
+});
